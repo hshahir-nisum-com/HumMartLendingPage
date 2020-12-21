c914ce1dc3e49470de4283e77a16e71ab91b5c73
Hafiz Shahir Zain <hshahir@nisum.com>
2020-12-21 17:54:18 +0500

    final , done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 more = document.getElementById('more').onmouseover = () =>{
     temp = document.getElementById('MoreBlock');
     temp.style.display = "flex";
@@ -6,7 +8,6 @@ more = document.getElementById('more').onmouseover = () =>{
         console.log(1)
     }
 }
-
 
 
 
@@ -22,5 +23,65 @@ function showSlides() {
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 1000); // Change image every 2 seconds
+  setTimeout(showSlides, 1000); // Change image every 1 seconds
 }
+
+
+
+function openNav() {
+document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+
+const flavoursContainer = document.getElementById('slider');
+const flavoursScrollWidth = flavoursContainer.scrollWidth;
+
+window.addEventListener('load', () => {
+  self.setInterval(() => {
+    if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
+      flavoursContainer.scrollTo(flavoursContainer.scrollLeft + 1, 0);
+    }
+  }, 100);
+});
+
+radio1 = document.getElementById("firstRadioBtn").checked = true
+
+function radioButton1(){
+
+  radio1 = document.getElementById("firstRadioBtn").checked
+  radio2 = document.getElementById("secondRadioBtn").checked
+
+if ( radio1){
+  console.log("radio1")
+  document.getElementById("secondRadioBtn").checked = false
+  
+    if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
+      flavoursContainer.scrollTo(200, 0);
+    }
+  }
+  
+
+}
+
+function radioButton2(){
+  radio1 = document.getElementById("firstRadioBtn").checked
+  radio2 = document.getElementById("secondRadioBtn").checked
+if (radio2){
+  console.log("radio2")
+  document.getElementById("firstRadioBtn").checked = false
+  if (flavoursContainer.scrollLeft !== flavoursScrollWidth) {
+    flavoursContainer.scrollTo(400, 200);
+  }
+}
+}
+
+
+
+
+
+
